f06b19776e3a641bb23f843f4c209cb5bbda5919
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2024-01-27 18:17:25 +0300

    Create tg_bot.py

---
+++ tg_bot.py
@@ -0,0 +1,45 @@
+import telebot
+import requests
+from main import send_message, hit, stand, GameData
+
+bot = telebot.TeleBot('6754252027:AAEkuhc8avy3HKpUo6xhZSeqLmlw4AN5pcE')
+game_data = GameData()
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Добро пожаловать в игру блэкджек!")
+
+
+@bot.message_handler(commands=['newgame'])
+def start_new_game(message):
+    global game_data
+    game_data = GameData()
+    game_data.deck_id =         requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').json()['deck_id']
+
+    send_message(message, "Новая игра началась! Используйте /hit, чтобы взять карту")
+
+
+@bot.message_handler(commands=['hit', 'stand'])
+def handle_commands(message):
+    global game_data
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_data = bot.get_chat_member(chat_id, user_id)
+    if user_data.status not in ["member", "administrator", "creator"]:
+        send_message(message, "Вы не можете использовать эти команды.")
+        return
+    command = message.text.lower()
+    if command == '/hit':
+        hit(game_data.deck_id, game_data.player_hand, message)
+    elif command == '/stand':
+        stand(game_data.deck_id, game_data.player_hand, game_data.dealer_hand, message)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.message:
+        pass
+
+
+bot.polling()
